5b1737b601521c15e6ea0c7997fad64a8368b950
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-11-04 11:53:53 -0600

    Converted to binary number

--- 10.BinaryNumbers/BinaryNumbers.js
+++ 10.BinaryNumbers/BinaryNumbers.js
@@ -1,6 +1,11 @@
-BinaryNumber(n){
-
-
+function BinaryNumber(n) {
+  let quotient = Math.floor(n / 2);
+  let remainder = n % 2;
+  if (n <= 1) {
+    return 1;
+  } else {
+    return BinaryNumber(quotient) + remainder.toString();
+  }
 }
 
-console.log(BinaryNumber(5));
+console.log(BinaryNumber(10));
