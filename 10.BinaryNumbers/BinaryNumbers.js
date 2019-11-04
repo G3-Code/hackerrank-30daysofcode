1d4dd7f7654ee590314e8d4ebdf15284c3b08a5c
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-11-04 16:05:59 -0600

    Binary Numbers - Solved

--- 10.BinaryNumbers/BinaryNumbers.js
+++ 10.BinaryNumbers/BinaryNumbers.js
@@ -8,4 +8,28 @@ function BinaryNumber(n) {
   }
 }
 
-console.log(BinaryNumber(10));
+let binaryVal = BinaryNumber(439);
+console.log(binaryVal);
+
+let binaryValArr = binaryVal.split("");
+let newArr = [];
+let count = 0;
+for (let i = 0; i < binaryValArr.length; i++) {
+  //   console.log(binaryValArr[i]);
+  if (binaryValArr[i] === "1") {
+    count++;
+    console.log(count);
+  } else {
+    if (count) {
+      newArr.push(count);
+      count = 0;
+    }
+  }
+}
+if (count) {
+  newArr.push(count);
+}
+newArr.sort(function(a, b) {
+  return b - a;
+});
+console.log(newArr[0]);
